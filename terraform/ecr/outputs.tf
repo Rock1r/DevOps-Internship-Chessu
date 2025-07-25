140c8cf75e1d32ddf818d3c7255ee72a1766ca60
Rostyslav <146858503+Rock1r@users.noreply.github.com>
2025-07-25 14:56:04 +0300

    adjusted readme.md

--- terraform/ecr/outputs.tf
+++ terraform/ecr/outputs.tf
@@ -5,3 +5,11 @@ output "server_repository_url" {
 output "client_repository_url" {
   value = aws_ecr_repository.client.repository_url
 } 
+
+output "client_repository_arn" {
+  value = aws_ecr_repository.client.arn
+}
+
+output "server_repository_arn" {
+  value = aws_ecr_repository.server.arn
+}
